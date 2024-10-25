00e1a22e876516dbaa04d550aad3c486c92e7b05
hasanovagil <agil.hasanov@ferrumcapital.az>
2024-10-26 00:10:03 +0400

    Develop Catalog.API Infrastructure, Handler and Endpoint Classes for CRUD

---
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -0,0 +1,8 @@
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Marten;
+global using Catalog.API.Models;
+global using BuildingBlocks.CQRS;
+global using Catalog.API.Exceptions;
+global using FluentValidation;
---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config => {
+        config.RegisterServicesFromAssembly(assembly);
+    });
+
+builder.Services.AddCarter();
+
+builder.Services.AddMarten(opts => { opts.Connection(builder.Configuration.GetConnectionString("Database")!); }).UseLightweightSessions();
+
+var app = builder.Build();
+
+app.MapCarter();
+
+app.Run();
